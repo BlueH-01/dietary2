4a204ac2a42766541fdde7b947dc00412a7937d9
hwang <dororojs@gmail.com>
2024-11-12 10:45:08 +0900

    login

--- lib/main.dart
+++ lib/main.dart
@@ -1,85 +1,25 @@
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import './firebase_init.dart'; // Firebase 초기화 관리 파일
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(MyApp());
+  WidgetsFlutterBinding.ensureInitialized(); // Flutter 바인딩 초기화
+  await FirebaseInit().initializeFirebase(); // Firebase 초기화
+  runApp(
+    const MyApp(),
+  ); // 앱 실행
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: SignUpScreen(),
-    );
-  }
-}
-
-class SignUpScreen extends StatefulWidget {
-  @override
-  _SignUpScreenState createState() => _SignUpScreenState();
-}
-
-class _SignUpScreenState extends State<SignUpScreen> {
-  final _auth = FirebaseAuth.instance;
-  final _emailController = TextEditingController();
-  final _passwordController = TextEditingController();
-  String _message = '';
-
-  Future<void> _signUp() async {
-    try {
-      final userCredential = await _auth.createUserWithEmailAndPassword(
-        email: _emailController.text,
-        password: _passwordController.text,
-      );
-      setState(() {
-        _message = '회원가입 성공! UID: ${userCredential.user?.uid}';
-      });
-    } catch (e) {
-      setState(() {
-        _message = '회원가입 실패: ${e.toString()}';
-      });
-    }
-  }
-
-  @override
-  void dispose() {
-    _emailController.dispose();
-    _passwordController.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("Firebase 회원가입 예제")),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextField(
-              controller: _emailController,
-              decoration: InputDecoration(labelText: "이메일"),
-              keyboardType: TextInputType.emailAddress,
-            ),
-            TextField(
-              controller: _passwordController,
-              decoration: InputDecoration(labelText: "비밀번호"),
-              obscureText: true,
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: _signUp,
-              child: Text("회원가입"),
-            ),
-            SizedBox(height: 20),
-            Text(_message, style: TextStyle(color: Colors.red)),
-          ],
-        ),
+      title: 'My InterViewer ', // 앱 제목
+      theme: ThemeData(
+        primarySwatch: Colors.blue, // 앱 테마
       ),
+      home: const LoginScreen(), // 시작 화면
     );
   }
 }
