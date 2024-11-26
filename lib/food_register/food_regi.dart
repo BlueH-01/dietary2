d25baa357e96ff20fbd1c55255e109203e592475
mamc <mamc325@kakao.com>
2024-11-26 11:30:32 +0900

    sung

--- lib/food_register/food_regi.dart
+++ lib/food_register/food_regi.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-
+//성현 작업파일
 class DietaryScreen extends StatefulWidget {
   const DietaryScreen({super.key});
 
