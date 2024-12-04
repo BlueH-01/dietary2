35ca596ae97fde21ea120b8c4a2c2801c7a3647c
ChMinki <147272296+ChMinki@users.noreply.github.com>
2024-12-05 08:48:16 +0900

    Create user_data_service.dart
    UserDataService Class 구현

---
+++ lib/data_mg/user_data_service.dart
@@ -0,0 +1,40 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class UserDataService {
+  final FirebaseFirestore firestore;
+
+  UserDataService(this.firestore);
+
+  // 사용자 데이터 실시간 스트림
+  Stream<Map<String, dynamic>> userDataStream(String userId) {
+    return firestore
+        .collection('users')
+        .doc(userId)
+        .snapshots()
+        .map((snapshot) {
+      if (snapshot.exists) {
+        return snapshot.data() as Map<String, dynamic>;
+      } else {
+        throw Exception("User data not found!");
+      }
+    });
+  }
+
+  // 사용자 데이터 단일 조회
+  Future<Map<String, dynamic>?> fetchUserData(String userId) async {
+    try {
+      DocumentSnapshot userDoc =
+          await firestore.collection('users').doc(userId).get();
+
+      if (userDoc.exists) {
+        return userDoc.data() as Map<String, dynamic>;
+      } else {
+        print('User data not found!');
+        return null;
+      }
+    } catch (e) {
+      print('Error fetching user data: $e');
+      return null;
+    }
+  }
+}
