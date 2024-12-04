79dc2f5cf7cdb7598b144aad978745366a7a66d3
ChMinki <147272296+ChMinki@users.noreply.github.com>
2024-12-05 06:51:22 +0900

    Update date_manager.dart
    DateManager에서 영양소 관련 로직 분리 및 활용 관련 변경

--- lib/data_mg/date_manager.dart
+++ lib/data_mg/date_manager.dart
@@ -1,89 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-//import '../data_mg/nutrition_manager.dart';
+import '../data_mg/nutrition_manager.dart';
 
 class DateManager {
   final FirebaseFirestore firestore; // Firestore 인스턴스
   final String userId; // 현재 사용자 ID
   DateTime selectedDate; // 현재 선택 날짜
 
-  // final NutritionManager nutritionManager; // NutritionManager 인스턴스
+  final NutritionManager nutritionManager; // NutritionManager 인스턴스
 
-  // 영양소 데이터
-  double calories = 0;
-  double carbs = 0;
-  double proteins = 0;
-  double fats = 0;
-
-  late Map<String, Map<String,dynamic>> mealData;
+  double get calories => nutritionManager.calories;
+  double get carbs => nutritionManager.carbs;
+  double get proteins => nutritionManager.proteins;
+  double get fats => nutritionManager.fats;
 
   DateManager({
     required this.firestore,
     required this.userId, // Id - 현재 사용자
     DateTime? initialDate, // 초기 날짜 - 오늘 날짜로 설정
-  }) : selectedDate = initialDate ?? DateTime.now() {
-    mealData = _initializeMealData();
-  }
+  }) : selectedDate = initialDate ?? DateTime.now(), 
+      nutritionManager = NutritionManager();
 
   // 현재 선택된 날짜의 포맷
   String formattedDate() {
     return "${selectedDate.toLocal()}".split(' ')[0];
   }
-
-      // 초기 식사 데이터 생성
-  Map<String, Map<String, dynamic>> _initializeMealData() {
-    return {
-      "아침": _emptyMeal(),
-      "점심": _emptyMeal(),
-      "저녁": _emptyMeal(),
-      "간식": _emptyMeal(),
-    };
-  }
-
-    // 빈 식사 데이터
-  Map<String, dynamic> _emptyMeal() {
-    return {
-      "name": "없음",
-      "calories": 0.0,
-      "carbs": 0.0,
-      "proteins": 0.0,
-      "fats": 0.0,
-    };
-  }
-
-  void _updateFromData(Map<String, dynamic> data) {
-    calories = (data['calories'] ?? 0).toDouble();
-    carbs = (data['carbs'] ?? 0).toDouble();
-    proteins = (data['proteins'] ?? 0).toDouble();
-    fats = (data['fats'] ?? 0).toDouble();
-    mealData = Map<String, Map<String, dynamic>>.from(
-      data['meals'] ?? _initializeMealData());
-  }
-
-  // 데이터 초기화
-  void resetData() {
-    calories = 0;
-    carbs = 0;
-    proteins = 0;
-    fats = 0;
-    mealData = _initializeMealData();
-  }
-
-  // 전체 합산 데이터 업데이트
-  void updateTotalData() {
-    calories = 0;
-    carbs = 0;
-    proteins = 0;
-    fats = 0;
-
-    mealData.forEach((key, value) {
-      calories += value['calories'];
-      carbs += value['carbs'];
-      proteins += value['proteins'];
-      fats += value['fats'];
-    });
-  }
-
 
   // Firestore에서 날짜별 데이터 불러오기
   Future<void> loadDataForDate({
@@ -100,15 +41,15 @@ class DateManager {
 
       if (doc.exists) {
         final data = doc.data()!;
-        _updateFromData(data);
+        nutritionManager.updateFromData(data);
         onDataLoaded(data);
       } else {
-        resetData();
+        nutritionManager.resetData();
         onNoData();
       }
     } catch (e) {
       debugPrint("Error loading data: $e");
-      resetData();
+      nutritionManager.resetData();
       onNoData();
     }
   }
@@ -116,14 +57,12 @@ class DateManager {
   // Firestore에 데이터 저장
   Future<void> saveDataForDate() async {
     final data = {
-      'calories': calories,
-      'carbs': carbs,
-      'proteins': proteins,
-      'fats': fats,
-      'meals': mealData,
-    };
-
-    try {
+      'calories': nutritionManager.calories,
+      'carbs': nutritionManager.carbs,
+      'proteins': nutritionManager.proteins,
+      'fats': nutritionManager.fats,
+      'meals': nutritionManager.mealData,
+    }; try {
       await firestore
           .collection('users')
           .doc(userId)
