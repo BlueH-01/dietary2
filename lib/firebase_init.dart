01976ede90facff1a7cae5b4f0e46dd89dbe5474
ChMinki <147272296+ChMinki@users.noreply.github.com>
2024-12-04 16:43:27 +0900

    Update firebase_init.dart

--- lib/firebase_init.dart
+++ lib/firebase_init.dart
@@ -1,13 +1,17 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart'; // FirebaseAuth 추가
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_storage/firebase_storage.dart'; // FirebaseStorage 추가
+import 'package:firebase_storage/firebase_storage.dart'; 
+// FirebaseStorage 추가
 
 class FirebaseInit {
   static final FirebaseInit _instance = FirebaseInit._internal();
   late FirebaseFirestore firestore;
   late FirebaseAuth auth; // FirebaseAuth 인스턴스 추가
   late FirebaseStorage storage; // FirebaseStorage 인스턴스 추가
+
+  // Singleton 접근을 위한 getter 추가
+  static FirebaseInit get instance => _instance;
 
   factory FirebaseInit() {
     return _instance;
